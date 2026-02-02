9a7eec1243ab0ef68c79cbdeeeaccdcbc129fb40
gisgcrs5 <gisdeveloper5.gcrs@gmail.com>
2026-02-02 10:57:52 +0530

    modified next config file

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/employee_management_dashboard',
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
-module.exports = nextConfig;
-
+export default nextConfig;
